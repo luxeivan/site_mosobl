c5c41912b094920f004f5c54b52036727a7b689d
luxeivan <luxeivan@gmail.com>
2022-11-01 11:33:45 +0300

    commit1133

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -16,7 +16,7 @@ export default function Main() {
       });
   }, []);
   return (
-    <motion.main className="page-grid__content" id="content" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
+    <>
       <div className="marquee-line marquee3k is-init" style={{ backgroundColor: "#E37021" }} data-speed="1">
         <div className="items-wrap">
           <div className="items marquee">
@@ -36,88 +36,90 @@ export default function Main() {
         </div>
       </div>
 
-      <section className="grid-posts">
-        <div className="container">
-          <div className="grid-posts__wrapper">
-            {publication &&
-              publication.map((item, index) => (
-                <div className="post-block" key={item.id}>
-                  {item.attributes.url&&item.attributes.url.includes("http") ? (
-                    <a className="post-block__wrapper" href={item.attributes.url}>
-                      {index < 4 ? (
-                        <div>
-                          <div className="post-block__bg"></div>
-                          <div className="post-block__bg-image"></div>
+      <motion.main className="page-grid__content" id="content" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
+        <section className="grid-posts">
+          <div className="container">
+            <div className="grid-posts__wrapper">
+              {publication &&
+                publication.map((item, index) => (
+                  <div className="post-block" key={item.id}>
+                    {item.attributes.url && item.attributes.url.includes("http") ? (
+                      <a className="post-block__wrapper" href={item.attributes.url}>
+                        {index < 4 ? (
+                          <div>
+                            <div className="post-block__bg"></div>
+                            <div className="post-block__bg-image"></div>
+                          </div>
+                        ) : (
+                          false
+                        )}
+                        <picture className="post-block__image-hover">
+                          <source srcSet={addressServer + item.attributes.image.data.attributes.url} type="image/webp" />
+                          {/* <source srcSet={imgc369cfb9b079726e4c79af4b2d7aff07} type="image/jpeg" /> */}
+                          <img src={addressServer + item.attributes.image.data.attributes.url} alt="ПЛАНОВЫЕ ОТКЛЮЧЕНИЯ" />
+                        </picture>
+                        <div className="post-block__wrap-img">
+                          <img className="post-block__img" src={addressServer + item.attributes.pic.data.attributes.url} alt="icon" />
                         </div>
-                      ) : (
-                        false
-                      )}
-                      <picture className="post-block__image-hover">
-                        <source srcSet={addressServer + item.attributes.image.data.attributes.url} type="image/webp" />
-                        {/* <source srcSet={imgc369cfb9b079726e4c79af4b2d7aff07} type="image/jpeg" /> */}
-                        <img src={addressServer + item.attributes.image.data.attributes.url} alt="ПЛАНОВЫЕ ОТКЛЮЧЕНИЯ" />
-                      </picture>
-                      <div className="post-block__wrap-img">
-                        <img className="post-block__img" src={addressServer + item.attributes.pic.data.attributes.url} alt="icon" />
-                      </div>
-                      <div className="post-block__wrap-text">
-                        <div className="post-block__up">
-                          <h4 className="post-block__caption">{item.attributes.title}</h4>
+                        <div className="post-block__wrap-text">
+                          <div className="post-block__up">
+                            <h4 className="post-block__caption">{item.attributes.title}</h4>
+                          </div>
+                          <div className="post-block__down">
+                            <p className="post-block__text">{item.attributes.description}</p>
+                            <button className="block-btn">
+                              <span className="block-btn__text">Смотреть</span>
+                              <div className="block-btn__wrap-svg">
+                                <svg className="block-btn__arrow">
+                                  <use href="/local/templates/vg/assets/img/arrow-link.svg#arrow-link"></use>
+                                </svg>
+                              </div>
+                            </button>
+                          </div>
                         </div>
-                        <div className="post-block__down">
-                          <p className="post-block__text">{item.attributes.description}</p>
-                          <button className="block-btn">
-                            <span className="block-btn__text">Смотреть</span>
-                            <div className="block-btn__wrap-svg">
-                              <svg className="block-btn__arrow">
-                                <use href="/local/templates/vg/assets/img/arrow-link.svg#arrow-link"></use>
-                              </svg>
-                            </div>
-                          </button>
+                      </a>
+                    ) : (
+                      <Link className="post-block__wrapper" to={item.attributes.url}>
+                        {index < 4 ? (
+                          <div>
+                            <div className="post-block__bg"></div>
+                            <div className="post-block__bg-image"></div>
+                          </div>
+                        ) : (
+                          false
+                        )}
+                        <picture className="post-block__image-hover">
+                          <source srcSet={addressServer + item.attributes.image.data.attributes.url} type="image/webp" />
+                          {/* <source srcSet={imgc369cfb9b079726e4c79af4b2d7aff07} type="image/jpeg" /> */}
+                          <img src={addressServer + item.attributes.image.data.attributes.url} alt="ПЛАНОВЫЕ ОТКЛЮЧЕНИЯ" />
+                        </picture>
+                        <div className="post-block__wrap-img">
+                          <img className="post-block__img" src={addressServer + item.attributes.pic.data.attributes.url} alt="icon" />
                         </div>
-                      </div>
-                    </a>
-                  ) : (
-                    <Link className="post-block__wrapper" to={item.attributes.url}>
-                      {index < 4 ? (
-                        <div>
-                          <div className="post-block__bg"></div>
-                          <div className="post-block__bg-image"></div>
+                        <div className="post-block__wrap-text">
+                          <div className="post-block__up">
+                            <h4 className="post-block__caption">{item.attributes.title}</h4>
+                          </div>
+                          <div className="post-block__down">
+                            <p className="post-block__text">{item.attributes.description}</p>
+                            <button className="block-btn">
+                              <span className="block-btn__text">Смотреть</span>
+                              <div className="block-btn__wrap-svg">
+                                <svg className="block-btn__arrow">
+                                  <use href="/local/templates/vg/assets/img/arrow-link.svg#arrow-link"></use>
+                                </svg>
+                              </div>
+                            </button>
+                          </div>
                         </div>
-                      ) : (
-                        false
-                      )}
-                      <picture className="post-block__image-hover">
-                        <source srcSet={addressServer + item.attributes.image.data.attributes.url} type="image/webp" />
-                        {/* <source srcSet={imgc369cfb9b079726e4c79af4b2d7aff07} type="image/jpeg" /> */}
-                        <img src={addressServer + item.attributes.image.data.attributes.url} alt="ПЛАНОВЫЕ ОТКЛЮЧЕНИЯ" />
-                      </picture>
-                      <div className="post-block__wrap-img">
-                        <img className="post-block__img" src={addressServer + item.attributes.pic.data.attributes.url} alt="icon" />
-                      </div>
-                      <div className="post-block__wrap-text">
-                        <div className="post-block__up">
-                          <h4 className="post-block__caption">{item.attributes.title}</h4>
-                        </div>
-                        <div className="post-block__down">
-                          <p className="post-block__text">{item.attributes.description}</p>
-                          <button className="block-btn">
-                            <span className="block-btn__text">Смотреть</span>
-                            <div className="block-btn__wrap-svg">
-                              <svg className="block-btn__arrow">
-                                <use href="/local/templates/vg/assets/img/arrow-link.svg#arrow-link"></use>
-                              </svg>
-                            </div>
-                          </button>
-                        </div>
-                      </div>
-                    </Link>
-                  )}
-                </div>
-              ))}
+                      </Link>
+                    )}
+                  </div>
+                ))}
+            </div>
           </div>
-        </div>
-      </section>
-    </motion.main>
+        </section>
+      </motion.main>
+    </>
   );
 }
