421fd9e500814fa2afb589355065bf41ecbb51a7
luxeivan <luxeivan@gmail.com>
2022-11-18 11:40:24 +0300

    commit 1140

--- src/pages/Filial.js
+++ src/pages/Filial.js
@@ -21,7 +21,6 @@ export default function Filial() {
         setFilial({});
       });
   }, []);
-  console.log(filial);
   return (
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
       <TopImage title={"Информация о компании"} />
