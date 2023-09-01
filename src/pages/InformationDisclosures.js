2b97c321bbc47a28152f7c910631a99a28d22c14
luxeivan <luxeivan@gmail.com>
2023-09-01 15:05:29 +0300

    commit1505

--- src/pages/InformationDisclosures.js
+++ src/pages/InformationDisclosures.js
@@ -23,6 +23,11 @@ export default function InformationDisclosures() {
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
       <TopImage image={imgb04877a3110d6b586d064fc3a2853c70} title={"Раскрытие информации"} />
       <div className="page-grid__content" id="content">
+        <p style={{ textAlign: "center" }}>
+          <a className="planned-notification__link" href="https://www.e-disclosure.ru/portal/company.aspx?id=15188" target="_blank">
+            Карточка компании АО "Мособлэнерго" на сайте центра раскрытия корпоративной информации
+          </a>
+        </p>
         <ul>
           {informationDisclosures &&
             informationDisclosures.map((item, index) => (
