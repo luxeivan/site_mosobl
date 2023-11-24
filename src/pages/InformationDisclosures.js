ed9394a08bd1bde748d58a9cbb7b609b2fbb5a5f
luxeivan <luxeivan@gmail.com>
2023-11-24 08:32:57 +0300

    commit0833

--- src/pages/InformationDisclosures.js
+++ src/pages/InformationDisclosures.js
@@ -23,6 +23,11 @@ export default function InformationDisclosures() {
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
       <TopImage image={imgb04877a3110d6b586d064fc3a2853c70} title={"Раскрытие информации"} />
       <div className="page-grid__content" id="content">
+        <p style={{ textAlign: "center",marginBottom:"10px" }}>
+          <a className="planned-notification__link" style={{border:"1px solid rgb(227, 112, 33)",boxShadow: "0px 0px 9px 3px rgba(227, 112, 33, 0.2)"}} href={`${addressServer}/uploads/20231123_Mosoblenergo_press_reliz_rus_5a57749dff.pdf?updated_at=2023-11-24T05:19:45.894Z`} target="_blank">
+            Кредитный рейтинг компании АО "Мособлэнерго" на уровне АА++(RU), прогноз «Стабильный»
+          </a>
+        </p>
         <p style={{ textAlign: "center" }}>
           <a className="planned-notification__link" href="https://www.e-disclosure.ru/portal/company.aspx?id=15188" target="_blank">
             Карточка компании АО "Мособлэнерго" на сайте центра раскрытия корпоративной информации
