d35d87ee08924e30562ab353c6d722313177c17b
Stanislav <yanut995@gmail.com>
2024-10-24 16:54:56 +0300

    Изменил Вакансии

--- src/pages/Vacancy/Vacancies.js
+++ src/pages/Vacancy/Vacancies.js
@@ -3,7 +3,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import TopImage from "../../components/TopImage";
 import vacan_img from "../../img/d21248be80705e7a80efdf5efde73cc5.jpg";
-import qr_rabota_ru from "../../img/qr_rabota_ru.svg";
+import qr_rabota_ru from "../../img/QR_HH.png";
 
 export default function Vacancies() {
   console.log("Test");
@@ -36,7 +36,7 @@ export default function Vacancies() {
           <div className="vacancies vacancies-main__item">
             <div className="vacancies__text-area">
               <h3>
-                Актуальные вакансии можно посмотреть на платформе Работа.ру{" "}
+                Актуальные вакансии можно посмотреть на платформе hh.ru{" "}
               </h3>
               <div className="vacancies__button-area">
                 {" "}
@@ -47,7 +47,7 @@ export default function Vacancies() {
                   target="_blank"
                   className="open-map__button"
                 >
-                  Перейти на Работа.ру
+                  Перейти на hh.ru
                 </a>
               </div>
             </div>
