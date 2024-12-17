49266d9d9487f5bfe025b9c929da98852072f4e6
Stanislav <yanut995@gmail.com>
2024-12-17 11:49:22 +0300

    Добавил верхний блок в практике

--- src/pages/Vacancy/Practice/PracticePart.js
+++ src/pages/Vacancy/Practice/PracticePart.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Typography, Flex } from "antd";
+import { Typography, Flex, Card, Button } from "antd";
 
-const { Title } = Typography;
+const { Title, Text } = Typography;
 
 export default function PracticePart() {
   return (
@@ -9,6 +9,25 @@ export default function PracticePart() {
       <Title level={2} style={{ textAlign: "center" }}>
         Практика
       </Title>
+      <Card
+        style={{
+          margin: 10,
+          padding: 20,
+          borderRadius: 10,
+          border: "2px solid #d9d9d9",
+          boxShadow: "0 2px 8px rgba(0, 0, 0, 0.1)",
+          position: "relative",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <Title
+          level={3}
+          style={{ margin: 0, textAlign: "center", width: "100%" }}
+        >
+          Приоритетными являются электроэнергетические направления обучения
+        </Title>
+      </Card>
     </Flex>
   );
 }
