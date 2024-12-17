3ec34037a86a6dd445bf81565a93044d3b448489
Stanislav <yanut995@gmail.com>
2024-12-17 10:02:06 +0300

    Обновил кнопку и перешел к блоку Практики

--- src/pages/Vacancy/Job/QrCodeHH.js
+++ src/pages/Vacancy/Job/QrCodeHH.js
@@ -23,7 +23,7 @@ export default function QrCodeHH() {
           <Button
             type="primary"
             href="https://hh.ru/employer/221874"
-            target="_blank"
+            style={{ color: "#fff" }} //Добавил потому что глобально там что-то перебивается
           >
             Перейти на hh.ru
           </Button>
@@ -33,40 +33,3 @@ export default function QrCodeHH() {
     </Card>
   );
 }
-
-// import React from "react";
-// import { Typography, Button, Flex, Image, Card } from "antd";
-// import qr_rabota_ru from "../../../img/QR_HH.png";
-
-// const { Title } = Typography;
-
-// export default function QrCodeHH() {
-//   return (
-//     <Card
-//       style={{
-//         padding: "10px",
-//         borderRadius: "10px",
-//         display: "flex",
-//         justifyContent: "center",
-//         alignItems: "center",
-//         border: "2px solid #d9d9d9",
-//         boxShadow: "0 2px 8px rgba(0, 0, 0, 0.1)",
-//       }}
-//     >
-//       <Flex vertical align="center" justify="center">
-//         <Title level={3} style={{ textAlign: "center" }}>
-//           Все вакансии
-//         </Title>
-//         <Button
-//           type="primary"
-//           href="https://hh.ru/employer/221874"
-//           target="_blank"
-//           style={{ marginBottom: "20px" }}
-//         >
-//           Перейти на hh.ru
-//         </Button>
-//         <Image src={qr_rabota_ru} alt="QR Code hh.ru" width={200} />
-//       </Flex>
-//     </Card>
-//   );
-// }
--- src/pages/Vacancy/Practice/PracticePart.js
+++ src/pages/Vacancy/Practice/PracticePart.js
@@ -1,19 +1,34 @@
 import React from "react";
-import { Typography, Space, Divider } from "antd";
-import VacanciesForm from "../VacanciesForm";
-import GrowthСhart from "./GrowthСhart";
+import { Typography, Flex } from "antd";
 
 const { Title } = Typography;
 
 export default function PracticePart() {
   return (
-    <Space direction="vertical" size="large" style={{ padding: "20px" }}>
-      <Title level={2}>Практика</Title>
-      <Title level={3}>Путь к успеху в Мособлэнерго</Title>
-      {/* График успехов */}
-      <GrowthСhart />
-      <Divider />
-      <VacanciesForm />
-    </Space>
+    <Flex vertical align="center" justify="center">
+      <Title level={2} style={{ textAlign: "center" }}>
+        Практика
+      </Title>
+    </Flex>
   );
 }
+
+// import React from "react";
+// import { Typography, Space, Divider } from "antd";
+// import VacanciesForm from "../VacanciesForm";
+// import GrowthСhart from "./GrowthСhart";
+
+// const { Title } = Typography;
+
+// export default function PracticePart() {
+//   return (
+//     <Space direction="vertical" size="large" style={{ padding: "20px" }}>
+//       <Title level={2}>Практика</Title>
+//       <Title level={3}>Путь к успеху в Мособлэнерго</Title>
+//       {/* График успехов */}
+//       <GrowthСhart />
+//       <Divider />
+//       <VacanciesForm />
+//     </Space>
+//   );
+// }
